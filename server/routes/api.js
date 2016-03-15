2a847f3e56e0bc04c6d0935948a71a0a0e8893f8
Y <jsslzrd@gmail.com>
2016-03-15 20:58:32 +0100

    Counter API

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,8 +4,15 @@ const routes = require('koa-router')();
 
 let counter = 0;
 
-routes.get('/counter', function *api() {
-  this.body = counter++;
+routes
+.get('/counter', function *api() {
+  this.body = counter;
+})
+.get('/counter/increment', function *api() {
+  this.body = ++counter;
+})
+.get('/counter/decrement', function *api() {
+  this.body = --counter;
 });
 
 module.exports = routes;
