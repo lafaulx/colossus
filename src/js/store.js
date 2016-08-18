6da7b05bf7beda6e1150a2e2621e28618596a741
Yaroslav Kravchenko <jsslzrd@gmail.com>
2016-08-18 23:08:31 +0200

    Added redux logger middleware

--- src/js/store.js
+++ src/js/store.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import counterReducer from './reducers/counter';
 import { apiMiddleware } from 'redux-api-middleware';
-
 import { routerReducer, routerMiddleware } from 'react-router-redux';
+import createLogger from 'redux-logger';
+
+import counterReducer from './reducers/counter';
 
 export function configureStore(history, initialState) {
   const reducer = combineReducers({
@@ -10,12 +11,15 @@ export function configureStore(history, initialState) {
     routing: routerReducer,
   });
 
+  const logger = createLogger();
+
   const store = createStore(
     reducer,
     initialState,
     applyMiddleware(
       apiMiddleware,
-      routerMiddleware(history)
+      routerMiddleware(history),
+      logger
     )
   );
 
